76db7524347a776b762fff0af47b3db8ac803297
Maxim Marquis <maxim.marquis@gmail.com>
2024-06-04 19:46:57 -0400

    My Simple Counter

---
+++ Fancy Counter/script.js
@@ -0,0 +1,26 @@
+const increaseButtonElement = document.querySelector('.counter__button--increase');
+const decreaseButtonElement = document.querySelector('.counter__button--decrease');
+const counterValueElement = document.querySelector('.counter__value');
+const counterResetElement = document.querySelector('.counter__reset')
+
+increaseButtonElement.addEventListener('click', function() {
+    // Get current value of counter
+    const currentValue = counterValueElement.textContent;
+    // convert value to number type
+    const currentValueAsNumber = +currentValue;
+    // Increment by 1
+    const newValue = currentValueAsNumber + 1; 
+    // Set counter element with new value
+    counterValueElement.textContent = newValue;
+})
+
+decreaseButtonElement.addEventListener('click', function () {
+    const currentValue = counterValueElement.textContent;
+    const currentValueAsNumber = +currentValue;
+    const decreasedValue = currentValueAsNumber - 1;
+    counterValueElement.textContent = decreasedValue;
+})
+
+counterResetElement.addEventListener('click', function(){
+    counterValueElement.textContent = 0;
+})
