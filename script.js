98ff06a6e9b0e9fc08ae661d90f1accd1d2b7ab7
Maxim Marquis <maxim.marquis@gmail.com>
2024-06-05 20:59:39 -0400

    Adding a limit to our counter

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
+const buttonsElement = document.querySelector('.counter__buttons');
 const increaseButtonElement = document.querySelector('.counter__button--increase');
 const decreaseButtonElement = document.querySelector('.counter__button--decrease');
 const counterValueElement = document.querySelector('.counter__value');
 const counterResetElement = document.querySelector('.counter__reset')
+const counterTitleElement = document.querySelector('.counter__title');
+const counterElement = document.querySelector('.counter');
 
-function incrementCounter() {
-     // Get current value of counter
-     const currentValue = counterValueElement.textContent;
-     // convert value to number type
-     const currentValueAsNumber = +currentValue;
-     // Increment by 1
-     const newValue = currentValueAsNumber + 1; 
-     // Set counter element with new value
-     counterValueElement.textContent = newValue;
+const incrementCounter = () => {
+    // Get current value of counter
+    const currentValue = counterValueElement.textContent;
+    // convert value to number type
+    const currentValueAsNumber = +currentValue;
+    // Increment by 1
+    let newValue = currentValueAsNumber + 1; 
+    //  if newValue is greater than 10, set it to 10, otherwise keep it like that
+    if (newValue > 10) {
+        newValue = 10
+        counterElement.classList.add('counter--limit');
+        counterTitleElement.innerHTML = "Limit! Buy <b>PRO</b> for > 10";
+        increaseButtonElement.disabled = true;
+        decreaseButtonElement.disabled = true;
+    }
+    // Set counter
+    counterValueElement.textContent = newValue;
+    // Unfocus button
+    increaseButtonElement.blur();
 }
 
 increaseButtonElement.addEventListener('click', incrementCounter)
 document.addEventListener("keydown", incrementCounter)
 
-decreaseButtonElement.addEventListener('click', function () {
+decreaseButtonElement.addEventListener('click', () => {
     const currentValue = counterValueElement.textContent;
     const currentValueAsNumber = +currentValue;
-    if (currentValueAsNumber > 0) {
-        const decreasedValue = currentValueAsNumber - 1;
-        counterValueElement.textContent = decreasedValue;
-    }
+    let decreasedValue = currentValueAsNumber - 1;
+    decreasedValue < 0 ? decreasedValue = 0 : decreasedValue;
+    counterValueElement.textContent = decreasedValue;
+    decreaseButtonElement.blur();
 })
 
-counterResetElement.addEventListener('click', function(){
+counterResetElement.addEventListener('click', () =>{
     counterValueElement.textContent = 0;
+    counterTitleElement.innerHTML = "Simple Counter";
+    counterElement.classList.remove('counter--limit')
+    increaseButtonElement.disabled = false;
+    decreaseButtonElement.disabled = false;
+    counterResetElement.blur();
 })
